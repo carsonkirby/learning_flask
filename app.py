901f4f789a96e5cce6e1e6a75e0093e4d1d890b9
Nikitha Nallure <145949444+nnallure2003@users.noreply.github.com>
2024-04-11 16:51:26 -0500

    commit

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# Route for the homepage
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route("/about")
+# Route for the about page
+@app.route('/about')
 def about():
-    return render_template("about.html")
-    
+    return render_template('about.html')
 
+# Route to delete "Mike" function, and the associated HTML template
+@app.route('/delete-mike')
+def delete_mike():
+    # Code to delete Mike goes here
+    return render_template('mike.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
